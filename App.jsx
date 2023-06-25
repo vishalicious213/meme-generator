66c4bc293734fbafc3ce00585bede02c87910721
vishalicious213 <vishalicious213@gmail.com>
2023-06-24 17:24:08 -0700

    drafted /components/Header and set up elements

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header"
+
 export default function App() {
     return (
         <>
-            <h1>App component</h1>
+            <Header />
         </>
     )
 }
---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img src="./troll-face.png" alt="troll face" />
+                <h2>Meme Generator</h2>
+            </div>
+            <div>React Course - Project 3</div>
+        </header>
+    )
+}
