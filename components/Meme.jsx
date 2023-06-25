97a1764b92be37bbf7c201302ec62edc26649f8d
vishalicious213 <vishalicious213@gmail.com>
2023-06-24 22:07:07 -0700

    Meme form set up with css grid

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,11 +1,19 @@
 export default function Meme() {
     return (
         <form>
-            <div className="inputs">
-                <input type="text" name="input-top" id="input-top" placeholder="Shut up" />
-                <input type="text" name="input-bot" id="input-bot" placeholder="and take my money" />
-            </div>
-            <button>Get a new meme image</button>
+            <input 
+                type="text" 
+                name="input-top" 
+                id="input-top" 
+                placeholder="Top text" 
+            />
+            <input 
+                type="text" 
+                name="input-bot" 
+                id="input-bot" 
+                placeholder="Bottom text" 
+            />
+            <button>Get a new meme image 🖼</button>
         </form>
     )
 }
