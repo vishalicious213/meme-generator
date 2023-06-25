f6becc6b5239c880a9a0d02254fe13dafda9995d
vishalicious213 <vishalicious213@gmail.com>
2023-06-24 22:29:08 -0700

    made form responsive

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,19 +1,21 @@
 export default function Meme() {
     return (
-        <form>
-            <input 
-                type="text" 
-                name="input-top" 
-                id="input-top" 
-                placeholder="Top text" 
-            />
-            <input 
-                type="text" 
-                name="input-bot" 
-                id="input-bot" 
-                placeholder="Bottom text" 
-            />
-            <button>Get a new meme image 🖼</button>
-        </form>
+        <main>
+            <form>
+                <input 
+                    type="text" 
+                    name="input-top" 
+                    id="input-top" 
+                    placeholder="Top text" 
+                />
+                <input 
+                    type="text" 
+                    name="input-bot" 
+                    id="input-bot" 
+                    placeholder="Bottom text" 
+                />
+                <button>Get a new meme image 🖼</button>
+            </form>
+        </main>
     )
 }
