27a21b5cdc9735a927679e61bbb4e3f48dfcc161
vishalicious213 <vishalicious213@gmail.com>
2023-07-03 14:19:43 -0700

    placed meme image in container and added top and bottom text. styled and formatted it at 320px

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -35,7 +35,11 @@ export default function Meme() {
                 <button type="button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </form>
 
-            <img src={meme.randomImage} className="meme-image" alt="meme-image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme-image" alt="meme-image" />
+                <h2 className="meme-text top">One does not simply</h2>
+                <h2 className="meme-text bottom">Walk into Mordor</h2>
+            </div>
         </main>
     )
 }
